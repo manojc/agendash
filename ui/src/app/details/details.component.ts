8833286eb23a54d1e2ca74ea6bb547cea006de1b
webuilderkrish <shubhamdigole@gmail.com>
2018-12-22 01:03:23 +0530

    test made some chnages to fet data by id but failed

--- ui/src/app/details/details.component.ts
+++ ui/src/app/details/details.component.ts
@@ -9,22 +9,27 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-  public uData = [];
-
+  public uData:any;
+  public test:any;
   constructor(private route: ActivatedRoute, private userData: UserDataService) { }
 
   ngOnInit() {
-
+    
     let jobId = this.route.snapshot.params.id;
     var currentTime = new Date();
     console.log(jobId, currentTime);
     this.userData.getData().subscribe(data => {
-      this.uData = data,
-        console.log(this.uData)
-
+      this.uData = data
+     
+        
+    
+  
+  
+     
+      
     });
-
-
-  }
+   
+    
+  } 
 
 }
